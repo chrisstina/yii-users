fefd9840b345527b8388ffcf217e556ffba7c0da
Christina Bratanova <chriss@Christina-Bratanovas-MacBook-Pro.local>
2014-10-13 20:22:16 +0300

    init commit

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\yiiusers;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\yiiusers\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
