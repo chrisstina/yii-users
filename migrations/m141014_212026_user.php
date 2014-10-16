128416fed4eae300f71211ada70e6a32f9739deb
Christina Bratanova <chriss@Christina-Bratanovas-MacBook-Pro.local>
2014-10-16 15:00:42 +0300

    migration fix

--- migrations/m141014_212026_user.php
+++ migrations/m141014_212026_user.php
@@ -11,7 +11,7 @@ class m141014_212026_user extends Migration
             'id' => 'INT UNSIGNED PRIMARY KEY AUTO_INCREMENT',
             'email' => 'VARCHAR(32) NOT NULL UNIQUE',
             'password_hash' => 'VARCHAR(60) NOT NULL UNIQUE',
-            'activation_code' => 'VARCHAR(32) NOT NULL UNIQUE',
+            'activation_code' => 'VARCHAR(32) NULL',
             'is_active' => 'BIT NOT NULL DEFAULT 0',
             'created_at' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP',
             'last_login_at' => 'TIMESTAMP NULL',
